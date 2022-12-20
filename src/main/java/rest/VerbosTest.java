e4ec3bb4673dd4a05353072bc6ac359d02d9d407
lfernandes25 <leanstorm@hotmail.com>
2022-12-20 12:54:41 -0300

    REST002 - Trabalhando com os verbos

--- src/main/java/rest/VerbosTest.java
+++ src/main/java/rest/VerbosTest.java
@@ -61,17 +61,81 @@ public class VerbosTest {
     @Test
     public void deveAlterarUsuario(){
         RestAssured.given()
-                .log().all()
-                .contentType("application/json")
-                .body("{\"name\": \"Jose\",\"age\": 50}")
+                    .log().all()
+                    .contentType(ContentType.JSON)
+                    .body("{\"name\": \"Usuario alterado\",\"age\": 50}")
                 .when()
-                .post("http://restapi.wcaquino.me/users/1")
+                    .put("http://restapi.wcaquino.me/users/1")
                 .then()
-                .log().all()
-                .statusCode(201)
-                .body("id", Matchers.is(Matchers.notNullValue()))
-                .body("name",Matchers.is("Jose"))
-                .body("age",Matchers.is(50))
+                    .log().all()
+                    .statusCode(200)
+                    .body("id", Matchers.is(1))
+                    .body("name",Matchers.is("Usuario alterado"))
+                    .body("age",Matchers.is(50))
+                    .body("salary", Matchers.is(1234.5677F))
+        ;
+    }
+
+    @Test
+    public void devoCostumizarUrl(){
+        RestAssured.given()
+                    .log().all()
+                    .contentType(ContentType.JSON)
+                    .body("{\"name\": \"Usuario alterado\",\"age\": 50}")
+                .when()
+                    .put("http://restapi.wcaquino.me/{entidade}/{userId}","users","1")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("id", Matchers.is(1))
+                    .body("name",Matchers.is("Usuario alterado"))
+                    .body("age",Matchers.is(50))
+                    .body("salary", Matchers.is(1234.5677F))
+        ;
+    }
+
+    @Test
+    public void devoCostumizarUrlParteDois(){
+        RestAssured.given()
+                    .log().all()
+                    .contentType(ContentType.JSON)
+                    .body("{\"name\": \"Usuario alterado\",\"age\": 50}")
+                    .pathParam("entidade","users")
+                    .pathParam("userId",1)
+                .when()
+                    .put("http://restapi.wcaquino.me/{entidade}/{userId}")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("id", Matchers.is(1))
+                    .body("name",Matchers.is("Usuario alterado"))
+                    .body("age",Matchers.is(50))
+                    .body("salary", Matchers.is(1234.5677F))
+        ;
+    }
+
+    @Test
+    public void deveRemoverUsuario(){
+        RestAssured.given()
+                    .log().all()
+                .when()
+                    .delete("http://restapi.wcaquino.me/users/1")
+                .then()
+                    .log().all()
+                    .statusCode(204)
+                ;
+    }
+
+    @Test
+    public void naoDeveRemoverUsuarioInexistente(){
+        RestAssured.given()
+                    .log().all()
+                .when()
+                    .delete("http://restapi.wcaquino.me/users/100201")
+                .then()
+                    .log().all()
+                    .statusCode(400)
+                    .body("error", Matchers.is("Registro inexistente"))
         ;
     }
 }
